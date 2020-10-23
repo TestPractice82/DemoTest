936f04c5e3452395fcc6abcc08be8f5023e00403
TestPractice82 <raghavenih.gmail.com>
2020-10-23 16:19:40 -0400

    first commit

---
+++ src/main/java/ToolTest/Demotest/App.java
@@ -0,0 +1,14 @@
+package ToolTest.Demotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("welcome");
+    }
+}
